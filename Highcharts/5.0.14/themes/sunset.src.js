2a6376f34972613de1ca2d610b5fccf512137488
Nazar Ugrinovsky <nazar9505@gmail.com>
2019-03-12 17:20:41 +0500

    update highcharts lib

--- Highcharts/5.0.14/themes/sunset.src.js
+++ Highcharts/5.0.14/themes/sunset.src.js
@@ -1,9 +1,53 @@
 /**
- * @license @product.name@ JS v@product.version@ (@product.date@)
+ * @license Highcharts JS v5.0.14 (2017-07-28)
  *
  * (c) 2009-2017 Highsoft AS
  *
  * License: www.highcharts.com/license
  */
 'use strict';
-import '../../themes/sunset.js';
+(function(factory) {
+    if (typeof module === 'object' && module.exports) {
+        module.exports = factory;
+    } else {
+        factory(Highcharts);
+    }
+}(function(Highcharts) {
+    (function(Highcharts) {
+        /**
+         * (c) 2010-2017 Highsoft AS
+         *
+         * License: www.highcharts.com/license
+         * 
+         * Accessible high-contrast theme for Highcharts. Considers colorblindness and 
+         * monochrome rendering.
+         * @author Øystein Moseng
+         */
+
+        Highcharts.theme = {
+            colors: ['#FDD089', '#FF7F79', '#A0446E', '#251535'],
+
+            colorAxis: {
+                maxColor: '#60042E',
+                minColor: '#FDD089'
+            },
+
+            plotOptions: {
+                map: {
+                    nullColor: '#fefefc'
+                }
+            },
+
+            navigator: {
+                series: {
+                    color: '#FF7F79',
+                    lineColor: '#A0446E'
+                }
+            }
+        };
+
+        // Apply the theme
+        Highcharts.setOptions(Highcharts.theme);
+
+    }(Highcharts));
+}));
