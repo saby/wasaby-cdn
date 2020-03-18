969b5eee9a4f4ca41ca47d65b28444222cc86d8b
nv.ugrinovskiy <nv.ugrinovskiy@tensor.ru>
2020-03-18 14:51:23 +0500

    https://online.sbis.ru/opendoc.html\?guid\=bf18714a-6195-48d4-95f2-9b203934b215

--- Highcharts/8.0.4/modules/overlapping-datalabels.js
+++ Highcharts/8.0.4/modules/overlapping-datalabels.js
@@ -5,4 +5,4 @@
 
  License: www.highcharts.com/license
 */
-(function(a){"object"===typeof module&&module.exports?(a["default"]=a,module.exports=a):"function"===typeof define&&define.amd?define("highcharts/modules/overlapping-datalabels",["highcharts"],function(b){a(b);a.Highcharts=b;return a}):a("undefined"!==typeof Highcharts?Highcharts:void 0)})(function(a){(function(a,c,d,e){a.hasOwnProperty(c)||(a[c]=e.apply(null,d))})(a?a._modules:{},"masters/modules/overlapping-datalabels.src.js",[],function(){})});
+(function(a){"object"===typeof module&&module.exports?(a["default"]=a,module.exports=a):"function"===typeof define&&define.amd?define(["/cdn/Highcharts/8.0.4/highcharts.js"],function(b){a(b);a.Highcharts=b;return a}):a("undefined"!==typeof Highcharts?Highcharts:void 0)})(function(a){(function(a,c,d,e){a.hasOwnProperty(c)||(a[c]=e.apply(null,d))})(a?a._modules:{},"masters/modules/overlapping-datalabels.src.js",[],function(){})});
