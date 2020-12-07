56ba2f46e2cdde8907edab031db4a69aefbfc4e4
aa.kornev <aa.kornev@tensor.ru>
2020-12-07 23:53:06 +0300

    https://online.sbis.ru/opendoc.html?guid=5d6d7a19-f3ff-4605-a290-035001e993e2

--- Maintenance/1.0.15/js/checkUrl.js
+++ Maintenance/1.0.15/js/checkUrl.js
@@ -1,9 +1,17 @@
-/**
-     * Проверяет параметр ret в адресной строке
-     * Если параметр валидная ссылка и ведет на наш сайт, 
-     * то происходит переход при включении пользователем js
-     */
-function getRedirectUrl(urlString, retPrefix) {
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        define([], factory);
+    } else if (typeof module === 'object' && module.exports) {
+        // Node. Не работает с CommonJS напрямую, 
+        // только CommonJS-образными средами, которые поддерживают      
+
+        // module.exports, как Node.
+        module.exports = factory();
+    } else {
+        // Глобальные переменные браузера (root это window)
+        root.getRedirectUrl = factory(root);
+    }
+}(this, function () {
     function removeURLParameter(url, parameter) {
         var urlparts = url.split('?');
         if (urlparts.length >= 2) {
@@ -33,21 +41,23 @@ function getRedirectUrl(urlString, retPrefix) {
         var reg = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#()?&//=]*)/;
         return reg.test(link);
     }
-    var locationHost;
-    if (isValidLink(urlString)) {
-        var link = document.createElement('a');
-        link.href = urlString;
-        locationHost = getSecondLevel(link.hostname)
-        var returnLink = urlString.indexOf(retPrefix) !== -1 ? urlString.split(retPrefix).pop() : '';
-        var validLink = getSecondLevel(locationHost);
+    /**
+     * Проверяет параметр ret в адресной строке
+     * Если параметр валидная ссылка и ведет на наш сайт, 
+     * то происходит переход при включении пользователем js
+     */
+    return function getRedirectUrl(currentLocation, retParamName) {
+        // tslint:disable-next-line: max-line-length
+        var returnLink = currentLocation.href.indexOf(`${retParamName}=`) !== -1 ? currentLocation.href.split(`${retParamName}=`).pop() : '';
+        var validLink = getSecondLevel(currentLocation.hostname)
         if (returnLink !== '') {
             if (isValidLink(returnLink)) {
                 var link = document.createElement('a');
                 link.href = returnLink;
-                var retLink = getSecondLevel(link.hostname)
+                var linkFromParams = getSecondLevel(link.hostname);
             }
         }
-        returnLink = validLink === retLink ? returnLink : removeURLParameter(urlString, 'ret');
+        returnLink = validLink === linkFromParams ? returnLink : removeURLParameter(currentLocation.href, retParamName);
+        return returnLink;
     }
-    return returnLink;
-}
+}));
