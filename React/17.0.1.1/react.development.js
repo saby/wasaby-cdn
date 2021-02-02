4f38aaf7e2f1c2c4f576151627394647ed349ddf
Ivan Mogilevsky <mogilevsky.ivan@gmail.com>
2021-02-02 17:45:17 +0300

    Revert "https://online.sbis.ru/opendoc.html?guid=e7e79a33-638e-4bee-888f-fe4bb15d1730"
    This reverts commit 10830ed5

--- React/17.0.1.1/react.development.js
+++ React/17.0.1.1/react.development.js
@@ -8,7 +8,7 @@
  */
 (function (global, factory) {
    typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
-      typeof define === 'function' && define.amd ? define('react', ['exports'], factory) :
+      typeof define === 'function' && define.amd ? define(['exports'], factory) :
          (global = global || self, factory(global.React = {}));
 }(this, (function (exports) { 'use strict';
 
