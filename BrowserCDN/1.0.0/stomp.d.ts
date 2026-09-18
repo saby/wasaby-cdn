99c8d4db62418f8be1083930538b077de72ed8e8
Служебный пользователь Инц №52255 <unix-prog_server@unix.tensor.ru@sbis-dev-mon.unix.tensor.ru>
2026-09-18 18:05:39 +0300

    new change

---
+++ BrowserCDN/1.0.0/stomp.d.ts
@@ -0,0 +1,90 @@
+// Type definitions for stompjs 2.3
+// Project: https://github.com/jmesnil/stomp-websocket
+// Definitions by: Jimi Charalampidis <https://github.com/jimic>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+/* eslint-disable */
+declare module Stomp {
+    export const VERSIONS: {
+        V1_0: string;
+        V1_1: string;
+        V1_2: string;
+        supportedVersions: () => string[];
+    };
+
+    export class Client {
+        connected: boolean;
+        counter: number;
+        heartbeat: {
+            incoming: number;
+            outgoing: number;
+        };
+        maxWebSocketFrameSize: number;
+        subscriptions: {};
+        ws: WebSocket;
+
+        debug(...args: string[]): any;
+
+        connect(
+            headers: { login: string; passcode: string; host?: string },
+            connectCallback: (frame?: Frame) => any,
+            errorCallback?: (error: string) => any
+        ): any;
+        connect(
+            login: string,
+            passcode: string,
+            connectCallback: (frame?: Frame) => any,
+            errorCallback?: (error: string) => any,
+            host?: string
+        ): any;
+
+        disconnect(disconnectCallback: () => any, headers?: {}): any;
+
+        send(destination: string, headers?: {}, body?: string): any;
+
+        subscribe(destination: string, callback?: (message: Message) => any, headers?: {}): any;
+
+        unsubscribe(id: string, headers?: { [key: string]: string }): any;
+
+        begin(transaction: string): any;
+
+        commit(transaction: string): any;
+
+        abort(transaction: string): any;
+
+        ack(messageID: string, subscription: string, headers?: {}): any;
+
+        nack(messageID: string, subscription: string, headers?: {}): any;
+    }
+
+    export interface Message {
+        command: string;
+        headers: Record<string, string>;
+        body: string;
+        ack(headers?: {}): any;
+        nack(headers?: {}): any;
+    }
+
+    export class Frame {
+        constructor(command: string, headers?: {}, body?: string);
+
+        toString(): string;
+
+        sizeOfUTF8(s: string): number;
+
+        unmarshall(datas: any): any;
+
+        marshall(command: string, headers?: {}, body?: string): any;
+    }
+
+    export function client(url: string, protocols?: string | string[]): Client;
+
+    export function over(ws: WebSocket): Client;
+
+    export function overTCP(host: string, port: number): Client;
+
+    export function overWS(url: string): Client;
+
+    export function setInterval(interval: number, f: (...args: any[]) => void): Number;
+
+    export function clearInterval(id: Number): void;
+}
---
+++ Maintenance/1.0.2/js/checkSoftware.js
@@ -0,0 +1,15 @@
+(function () {
+   var userAgent = window.navigator.userAgent;
+   var userOs = OS.detect(userAgent);
+   var userBrowser = Browser.detect(userAgent);
+   if (!userOs.isAvailable || !userBrowser.isAvailable) {
+      redirect();
+   }
+
+   function redirect() {
+      var RETURN_LINK_PREFIX = '?ret=';
+      var DIAGNOSTIC_PAGE = '/cdn/Maintenance/1.0.4/html/diagnostic.html';
+      var returnLink = window.location.href.split(window.location.host).pop();
+      window.location.href = window.location.protocol + '//' + window.location.host + DIAGNOSTIC_PAGE + RETURN_LINK_PREFIX + returnLink;
+   }
+}());
